c3160142bda9a85f2d2d7d4b4916e4741d2d333b
yaskazeko <yaskazeko@gmail.com>
2020-05-06 16:12:48 +0300

    refactoring

--- backup_home.py
+++ backup_home.py
@@ -1,18 +1,32 @@
 #! python3
 
-import zipfile, os
+import os
+import zipfile
 
-def backupToZip(folder):
-    folder=os.path.abspath(folder)
-    number=1
+
+def backup_to_zip(folder):
+    print("test")
+    folder = os.path.abspath(folder)
+    number = 1
     while True:
-        zipFilename=os.path.basename(folder)+''+str(number)+'.zip'
-        if not os.path.exists(zipFilename):
+        zip_filename = os.path.basename(folder) + '' + str(number) + '.zip'
+        if not os.path.exists(zip_filename):
             break
-        number=number+1
-#add zip-file
-    print('file is created with name %s...:' % (zipFilename))
-    backupZip = zipfile.ZipFile(zipFilename, 'w')
+        number = number + 1
+    # add zip-file
+    print('file is created with name %s...:' % zip_filename)
+    backup_zip = zipfile.ZipFile(zip_filename, 'w')
+    #    print('Done')
+    # walk
+    for foldername, subfolders, filenames in os.walk(folder):
+        print('adding files from folder %s...' % folder)
+        backup_zip.write(folder)
+        for filename in filenames:
+            if filename.startswith(os.path.basename(folder) + '_') and filename.endswith('.zip'):
+                continue
+            backup_zip.write(os.path.join(folder, filename))
+    backup_zip.close()
     print('Done')
 
-backupToZip('/tmp')
+
+backup_to_zip('~/updater/')
